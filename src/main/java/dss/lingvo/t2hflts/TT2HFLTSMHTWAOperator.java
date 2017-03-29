0a8e435545f3ce4ec3e085421a8221fd81141874
Alexander Demidovskij <alexander.demidovskij@intel.com>
2017-03-29 11:05:02 +0300

    [LDSS-4] refactored the mhtwowa operator

--- src/main/java/dss/lingvo/t2hflts/TT2HFLTSMHTWAOperator.java
+++ src/main/java/dss/lingvo/t2hflts/TT2HFLTSMHTWAOperator.java
@@ -10,39 +10,6 @@ public class TT2HFLTSMHTWAOperator {
     public TT2HFLTS calculate(List<TT2HFLTS> sets, float[] weights, int targetScaleSize) {
         TT2HFLTSMTWAOperator mtwaOperator = new TT2HFLTSMTWAOperator();
 
-        int expectedQuantity = (int) Arrays
-                .stream(sets.toArray())
-                .reduce(1, (el, partial) -> (int) el * ((TT2HFLTS) partial).getSize());
-
-        ArrayList<ArrayList<TTTuple>> store = new ArrayList<>();
-        for (int i = 0; i < expectedQuantity; i++) {
-            store.add(new ArrayList<>());
-        }
-
-        for (TT2HFLTS set : sets) {
-            int shift = 0;
-            for (int i = 0; i < expectedQuantity; i++) {
-                int actualIndex = i % set.getSize() + shift;
-                store.get(i).add(set.getTerms().get(actualIndex));
-            }
-            shift++;
-        }
-//
-//        int [] depth = {2, 2, 2};
-//        int shift = 0;
-//        for (TT2HFLTS set : sets) {
-//            // iterate through the list 1st level, now the element is set
-//
-//            for (int i = 0; i < expectedQuantity; i++) {
-//                int actualIndex = i % set.getSize()+shift;
-////                if (actualIndex == set.getTerms().size()-1){
-////                    actualIndex = 0;
-////                }
-//                store.get(i).add(set.getTerms().get(actualIndex));
-//            }
-//            shift++;
-//        }
-
         ArrayList<TTTuple> input = new ArrayList<>();
         for (int i = 0; i < sets.size(); ++i) {
             for (int index = 0; index < sets.get(i).getTerms().size(); index++) {
@@ -55,15 +22,36 @@ public class TT2HFLTSMHTWAOperator {
             sizes[i] = sets.get(i).getSize();
         }
 
-//        int[] input = {10, 20, 30, 40, 50};    // input array
-        int k = sets.size();                             // sequence length
+        List<int[]> subsets = getAllPossibleCombinations(sets.size(), input.size());
 
+        ArrayList<ArrayList<TTTuple>> actualSubsets = eliminateImpossibleCombinations(subsets, sizes, input);
+
+        ArrayList<TT2HFLTS> t2HFLTSstore = new ArrayList<>();
+        for (ArrayList<TTTuple> est : actualSubsets) {
+            t2HFLTSstore.add(new TT2HFLTS(est));
+        }
+
+        ArrayList<TTTuple> res = new ArrayList<>();
+        for (TT2HFLTS el : t2HFLTSstore) {
+            res.add(mtwaOperator.calculate(el.getTerms(), weights, targetScaleSize));
+        }
+        List<TTTuple> sortedRes = TTUtils.sortTuples(res, false);
+        return new TT2HFLTS(sortedRes);
+    }
+
+    /**
+     * To get all possible combination of indexes
+     * @param k  - sequence length
+     * @param inputSize
+     * @return
+     */
+    private List<int[]> getAllPossibleCombinations(int k, int inputSize){
         List<int[]> subsets = new ArrayList<>();
 
         int[] s = new int[k];                  // here we'll keep indices
         // pointing to elements in input array
 
-        if (k <= input.size()) {
+        if (k <= inputSize) {
             // first index sequence: 0, 1, 2, ...
             for (int i = 0; (s[i] = i) < k - 1; i++) ;
             int[] tmpArr = new int[k];
@@ -72,7 +60,7 @@ public class TT2HFLTSMHTWAOperator {
             for (; ; ) {
                 int i;
                 // find position of item that can be incremented
-                for (i = k - 1; i >= 0 && s[i] == input.size() - k + i; i--) ;
+                for (i = k - 1; i >= 0 && s[i] == inputSize - k + i; i--) ;
                 if (i < 0) {
                     break;
                 } else {
@@ -86,14 +74,22 @@ public class TT2HFLTSMHTWAOperator {
                 }
             }
         }
+        return subsets;
+    }
 
-        System.out.println(subsets);
 
-        // after we have got all the combinations, we need to sort out wrong
-        // because some of combinations are from one set
-
+    /**
+     * after we have got all the combinations, we need to sort out wrong
+     * because some of combinations are from one set
+     * @param subsets - combinations of indexes
+     * @param sizes - sizes of TT2HFLTS to get the shift in indexes
+     * @param input - all ttuples listeed in one vector
+     * @return
+     */
+    private ArrayList<ArrayList<TTTuple>> eliminateImpossibleCombinations(List<int[]> subsets,
+                                                                          int[] sizes,
+                                                                          ArrayList<TTTuple> input){
         ArrayList<ArrayList<TTTuple>> actualSubsets = new ArrayList<>();
-
         for(int [] subset : subsets){
             boolean isOK = true;
             for (int positionInSubset = 0; positionInSubset < subset.length; ++positionInSubset){
@@ -119,18 +115,6 @@ public class TT2HFLTSMHTWAOperator {
                 actualSubsets.add(listToAdd);
             }
         }
-
-
-        ArrayList<TT2HFLTS> t2HFLTSstore = new ArrayList<>();
-        for (ArrayList<TTTuple> est : actualSubsets) {
-            t2HFLTSstore.add(new TT2HFLTS(est));
-        }
-
-        ArrayList<TTTuple> res = new ArrayList<>();
-        for (TT2HFLTS el : t2HFLTSstore) {
-            res.add(mtwaOperator.calculate(el.getTerms(), weights, targetScaleSize));
-        }
-        List<TTTuple> sortedRes = TTUtils.sortTuples(res, false);
-        return new TT2HFLTS(sortedRes);
+        return actualSubsets;
     }
 }
