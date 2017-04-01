74bbcf2707c04534fd38dd7179d5d88a3c3e5a35
Alexander Demidovskij <alexander.demidovskij@intel.com>
2017-04-01 16:29:25 +0300

    [LDSS-4] revelaed the problem with the third column. enabled reading from file

--- src/main/java/dss/lingvo/t2hflts/TT2HFLTSCoordinator.java
+++ src/main/java/dss/lingvo/t2hflts/TT2HFLTSCoordinator.java
@@ -6,7 +6,7 @@ import dss.lingvo.t2.TTNormalizedTranslator;
 import dss.lingvo.t2.TTTuple;
 import dss.lingvo.utils.TTJSONReader;
 import dss.lingvo.utils.TTUtils;
-import dss.lingvo.utils.models.TTJSONModel;
+import dss.lingvo.utils.models.*;
 
 import java.io.IOException;
 import java.util.*;
@@ -120,370 +120,59 @@ public class TT2HFLTSCoordinator {
 
         // 1. Gather feedback and translate directly to the TT2HFLTS (per each expert)
 
-        /**
-         * Expert #1
-         */
-        // expert 1, alternative 1
-        ArrayList<TTTuple> exp1alt1crit1List = new ArrayList<>();
-        exp1alt1crit1List.add(new TTTuple("4", 7, 0, 4));
-        exp1alt1crit1List.add(new TTTuple("5", 7, 0, 5));
-
-        TT2HFLTS exp1alt1crit1 = new TT2HFLTS(exp1alt1crit1List);
-
-        ArrayList<TTTuple> exp1alt1crit2List = new ArrayList<>();
-        exp1alt1crit2List.add(new TTTuple("3", 7, 0, 3));
-
-        TT2HFLTS exp1alt1crit2 = new TT2HFLTS(exp1alt1crit2List);
-
-        ArrayList<TTTuple> exp1alt1crit3List = new ArrayList<>();
-        exp1alt1crit3List.add(new TTTuple("4", 7, 0, 4));
-
-        TT2HFLTS exp1alt1crit3 = new TT2HFLTS(exp1alt1crit3List);
-
-        // expert 1, alternative 2
-        ArrayList<TTTuple> exp1alt2crit1List = new ArrayList<>();
-        exp1alt2crit1List.add(new TTTuple("2", 7, 0, 2));
-
-        TT2HFLTS exp1alt2crit1 = new TT2HFLTS(exp1alt2crit1List);
-
-        ArrayList<TTTuple> exp1alt2crit2List = new ArrayList<>();
-        exp1alt2crit2List.add(new TTTuple("5", 7, 0, 5));
-        exp1alt2crit2List.add(new TTTuple("6", 7, 0, 6));
-
-        TT2HFLTS exp1alt2crit2 = new TT2HFLTS(exp1alt2crit2List);
-
-        ArrayList<TTTuple> exp1alt2crit3List = new ArrayList<>();
-        exp1alt2crit3List.add(new TTTuple("3", 7, 0, 3));
-
-        TT2HFLTS exp1alt2crit3 = new TT2HFLTS(exp1alt2crit3List);
-
-        // expert 1, alternative 3
-        ArrayList<TTTuple> exp1alt3crit1List = new ArrayList<>();
-        exp1alt3crit1List.add(new TTTuple("1", 7, 0, 1));
-
-        TT2HFLTS exp1alt3crit1 = new TT2HFLTS(exp1alt3crit1List);
-
-        ArrayList<TTTuple> exp1alt3crit2List = new ArrayList<>();
-        exp1alt3crit2List.add(new TTTuple("5", 7, 0, 5));
-
-        TT2HFLTS exp1alt3crit2 = new TT2HFLTS(exp1alt3crit2List);
-
-        ArrayList<TTTuple> exp1alt3crit3List = new ArrayList<>();
-        exp1alt3crit3List.add(new TTTuple("3", 7, 0, 3));
-        exp1alt3crit3List.add(new TTTuple("4", 7, 0, 4));
-
-        TT2HFLTS exp1alt3crit3 = new TT2HFLTS(exp1alt3crit3List);
-
-        // expert 1, alternative 4
-        ArrayList<TTTuple> exp1alt4crit1List = new ArrayList<>();
-        exp1alt4crit1List.add(new TTTuple("5", 7, 0, 5));
-        exp1alt4crit1List.add(new TTTuple("6", 7, 0, 6));
-
-        TT2HFLTS exp1alt4crit1 = new TT2HFLTS(exp1alt4crit1List);
-
-        ArrayList<TTTuple> exp1alt4crit2List = new ArrayList<>();
-        exp1alt4crit2List.add(new TTTuple("4", 7, 0, 4));
-
-        TT2HFLTS exp1alt4crit2 = new TT2HFLTS(exp1alt4crit2List);
-
-        ArrayList<TTTuple> exp1alt4crit3List = new ArrayList<>();
-        exp1alt4crit3List.add(new TTTuple("3", 7, 0, 3));
-
-        TT2HFLTS exp1alt4crit3 = new TT2HFLTS(exp1alt4crit3List);
-
-        // expert 1, alternative 5
-        ArrayList<TTTuple> exp1alt5crit1List = new ArrayList<>();
-        exp1alt5crit1List.add(new TTTuple("3", 7, 0, 3));
-
-        TT2HFLTS exp1alt5crit1 = new TT2HFLTS(exp1alt5crit1List);
-
-        ArrayList<TTTuple> exp1alt5crit2List = new ArrayList<>();
-        exp1alt5crit2List.add(new TTTuple("1", 7, 0, 1));
-
-        TT2HFLTS exp1alt5crit2 = new TT2HFLTS(exp1alt5crit2List);
-
-        ArrayList<TTTuple> exp1alt5crit3List = new ArrayList<>();
-        exp1alt5crit3List.add(new TTTuple("5", 7, 0, 5));
-        exp1alt5crit3List.add(new TTTuple("6", 7, 0, 6));
-
-        TT2HFLTS exp1alt5crit3 = new TT2HFLTS(exp1alt5crit3List);
-
-        ArrayList<TT2HFLTS> exp1alt1 = new ArrayList<>();
-        exp1alt1.add(exp1alt1crit1);
-        exp1alt1.add(exp1alt1crit2);
-        exp1alt1.add(exp1alt1crit3);
-
-        ArrayList<TT2HFLTS> exp1alt2 = new ArrayList<>();
-        exp1alt2.add(exp1alt2crit1);
-        exp1alt2.add(exp1alt2crit2);
-        exp1alt2.add(exp1alt2crit3);
-
-        ArrayList<TT2HFLTS> exp1alt3 = new ArrayList<>();
-        exp1alt3.add(exp1alt3crit1);
-        exp1alt3.add(exp1alt3crit2);
-        exp1alt3.add(exp1alt3crit3);
-
-        ArrayList<TT2HFLTS> exp1alt4 = new ArrayList<>();
-        exp1alt4.add(exp1alt4crit1);
-        exp1alt4.add(exp1alt4crit2);
-        exp1alt4.add(exp1alt4crit3);
-
-        ArrayList<TT2HFLTS> exp1alt5 = new ArrayList<>();
-        exp1alt5.add(exp1alt5crit1);
-        exp1alt5.add(exp1alt5crit2);
-        exp1alt5.add(exp1alt5crit3);
-
-        ArrayList<ArrayList<TT2HFLTS>> exp1 = new ArrayList<>();
-        exp1.add(exp1alt1);
-        exp1.add(exp1alt2);
-        exp1.add(exp1alt3);
-        exp1.add(exp1alt4);
-        exp1.add(exp1alt5);
-
-        /**
-         * Expert #2
-         */
-        // expert 2, alternative 1
-        ArrayList<TTTuple> exp2alt1crit1List = new ArrayList<>();
-        exp2alt1crit1List.add(new TTTuple("5", 7, 0, 5));
-
-        TT2HFLTS exp2alt1crit1 = new TT2HFLTS(exp2alt1crit1List);
-
-        ArrayList<TTTuple> exp2alt1crit2List = new ArrayList<>();
-        exp2alt1crit2List.add(new TTTuple("2", 7, 0, 2));
-
-        TT2HFLTS exp2alt1crit2 = new TT2HFLTS(exp2alt1crit2List);
-
-        ArrayList<TTTuple> exp2alt1crit3List = new ArrayList<>();
-        exp2alt1crit3List.add(new TTTuple("3", 7, 0, 3));
-        exp2alt1crit3List.add(new TTTuple("4", 7, 0, 4));
-
-        TT2HFLTS exp2alt1crit3 = new TT2HFLTS(exp2alt1crit3List);
-
-        // expert 2, alternative 2
-        ArrayList<TTTuple> exp2alt2crit1List = new ArrayList<>();
-        exp2alt2crit1List.add(new TTTuple("3", 7, 0, 3));
-        exp2alt2crit1List.add(new TTTuple("4", 7, 0, 4));
-
-        TT2HFLTS exp2alt2crit1 = new TT2HFLTS(exp2alt2crit1List);
-
-        ArrayList<TTTuple> exp2alt2crit2List = new ArrayList<>();
-        exp2alt2crit2List.add(new TTTuple("4", 7, 0, 4));
-        exp2alt2crit2List.add(new TTTuple("5", 7, 0, 5));
-
-        TT2HFLTS exp2alt2crit2 = new TT2HFLTS(exp2alt2crit2List);
-
-        ArrayList<TTTuple> exp2alt2crit3List = new ArrayList<>();
-        exp2alt2crit3List.add(new TTTuple("2", 7, 0, 2));
-
-        TT2HFLTS exp2alt2crit3 = new TT2HFLTS(exp2alt2crit3List);
-
-        // expert 2, alternative 3
-        ArrayList<TTTuple> exp2alt3crit1List = new ArrayList<>();
-        exp2alt3crit1List.add(new TTTuple("2", 7, 0, 2));
-
-        TT2HFLTS exp2alt3crit1 = new TT2HFLTS(exp2alt3crit1List);
-
-        ArrayList<TTTuple> exp2alt3crit2List = new ArrayList<>();
-        exp2alt3crit2List.add(new TTTuple("3", 7, 0, 3));
-        exp2alt3crit2List.add(new TTTuple("4", 7, 0, 4));
-
-        TT2HFLTS exp2alt3crit2 = new TT2HFLTS(exp2alt3crit2List);
-
-        ArrayList<TTTuple> exp2alt3crit3List = new ArrayList<>();
-        exp2alt3crit3List.add(new TTTuple("3", 7, 0, 3));
-        exp2alt3crit3List.add(new TTTuple("4", 7, 0, 4));
-
-        TT2HFLTS exp2alt3crit3 = new TT2HFLTS(exp2alt3crit3List);
-
-        // expert 2, alternative 4
-        ArrayList<TTTuple> exp2alt4crit1List = new ArrayList<>();
-        exp2alt4crit1List.add(new TTTuple("5", 7, 0, 5));
-
-        TT2HFLTS exp2alt4crit1 = new TT2HFLTS(exp2alt4crit1List);
-
-        ArrayList<TTTuple> exp2alt4crit2List = new ArrayList<>();
-        exp2alt4crit2List.add(new TTTuple("5", 7, 0, 5));
-        exp2alt4crit2List.add(new TTTuple("6", 7, 0, 6));
-
-        TT2HFLTS exp2alt4crit2 = new TT2HFLTS(exp2alt4crit2List);
-
-        ArrayList<TTTuple> exp2alt4crit3List = new ArrayList<>();
-        exp2alt4crit3List.add(new TTTuple("2", 7, 0, 2));
-
-        TT2HFLTS exp2alt4crit3 = new TT2HFLTS(exp2alt4crit3List);
-
-        // expert 2, alternative 5
-        ArrayList<TTTuple> exp2alt5crit1List = new ArrayList<>();
-        exp2alt5crit1List.add(new TTTuple("3", 7, 0, 3));
-
-        TT2HFLTS exp2alt5crit1 = new TT2HFLTS(exp2alt5crit1List);
-
-        ArrayList<TTTuple> exp2alt5crit2List = new ArrayList<>();
-        exp2alt5crit2List.add(new TTTuple("2", 7, 0, 2));
-
-        TT2HFLTS exp2alt5crit2 = new TT2HFLTS(exp2alt5crit2List);
-
-        ArrayList<TTTuple> exp2alt5crit3List = new ArrayList<>();
-        exp2alt5crit3List.add(new TTTuple("5", 7, 0, 5));
-
-        TT2HFLTS exp2alt5crit3 = new TT2HFLTS(exp2alt5crit3List);
-
-        ArrayList<TT2HFLTS> exp2alt1 = new ArrayList<>();
-        exp2alt1.add(exp2alt1crit1);
-        exp2alt1.add(exp2alt1crit2);
-        exp2alt1.add(exp2alt1crit3);
-
-        ArrayList<TT2HFLTS> exp2alt2 = new ArrayList<>();
-        exp2alt2.add(exp2alt2crit1);
-        exp2alt2.add(exp2alt2crit2);
-        exp2alt2.add(exp2alt2crit3);
-
-        ArrayList<TT2HFLTS> exp2alt3 = new ArrayList<>();
-        exp2alt3.add(exp2alt3crit1);
-        exp2alt3.add(exp2alt3crit2);
-        exp2alt3.add(exp2alt3crit3);
-
-        ArrayList<TT2HFLTS> exp2alt4 = new ArrayList<>();
-        exp2alt4.add(exp2alt4crit1);
-        exp2alt4.add(exp2alt4crit2);
-        exp2alt4.add(exp2alt4crit3);
-
-        ArrayList<TT2HFLTS> exp2alt5 = new ArrayList<>();
-        exp2alt5.add(exp2alt5crit1);
-        exp2alt5.add(exp2alt5crit2);
-        exp2alt5.add(exp2alt5crit3);
-
-        ArrayList<ArrayList<TT2HFLTS>> exp2 = new ArrayList<>();
-        exp2.add(exp2alt1);
-        exp2.add(exp2alt2);
-        exp2.add(exp2alt3);
-        exp2.add(exp2alt4);
-        exp2.add(exp2alt5);
-
-        /**
-         * Expert #3
-         */
-        // expert 3, alternative 1
-        ArrayList<TTTuple> exp3alt1crit1List = new ArrayList<>();
-        exp3alt1crit1List.add(new TTTuple("4", 5, 0, 4));
-
-        TT2HFLTS exp3alt1crit1 = new TT2HFLTS(exp3alt1crit1List);
-
-        ArrayList<TTTuple> exp3alt1crit2List = new ArrayList<>();
-        exp3alt1crit2List.add(new TTTuple("2", 5, 0, 2));
-
-        TT2HFLTS exp3alt1crit2 = new TT2HFLTS(exp3alt1crit2List);
-
-        ArrayList<TTTuple> exp3alt1crit3List = new ArrayList<>();
-        exp3alt1crit3List.add(new TTTuple("2", 5, 0, 2));
-        exp3alt1crit3List.add(new TTTuple("3", 5, 0, 3));
-
-        TT2HFLTS exp3alt1crit3 = new TT2HFLTS(exp3alt1crit3List);
-
-        // expert 3, alternative 2
-        ArrayList<TTTuple> exp3alt2crit1List = new ArrayList<>();
-        exp3alt2crit1List.add(new TTTuple("2", 5, 0, 2));
-
-        TT2HFLTS exp3alt2crit1 = new TT2HFLTS(exp3alt2crit1List);
-
-        ArrayList<TTTuple> exp3alt2crit2List = new ArrayList<>();
-        exp3alt2crit2List.add(new TTTuple("4", 5, 0, 4));
-
-        TT2HFLTS exp3alt2crit2 = new TT2HFLTS(exp3alt2crit2List);
-
-        ArrayList<TTTuple> exp3alt2crit3List = new ArrayList<>();
-        exp3alt2crit3List.add(new TTTuple("2", 5, 0, 2));
-
-        TT2HFLTS exp3alt2crit3 = new TT2HFLTS(exp3alt2crit3List);
-
-        // expert 3, alternative 3
-        ArrayList<TTTuple> exp3alt3crit1List = new ArrayList<>();
-        exp3alt3crit1List.add(new TTTuple("2", 5, 0, 2));
-
-        TT2HFLTS exp3alt3crit1 = new TT2HFLTS(exp3alt3crit1List);
-
-        ArrayList<TTTuple> exp3alt3crit2List = new ArrayList<>();
-        exp3alt3crit2List.add(new TTTuple("3", 5, 0, 3));
-
-        TT2HFLTS exp3alt3crit2 = new TT2HFLTS(exp3alt3crit2List);
-
-        ArrayList<TTTuple> exp3alt3crit3List = new ArrayList<>();
-        exp3alt3crit3List.add(new TTTuple("2", 5, 0, 2));
-        exp3alt3crit3List.add(new TTTuple("3", 5, 0, 3));
-
-        TT2HFLTS exp3alt3crit3 = new TT2HFLTS(exp3alt3crit3List);
-
-        // expert 3, alternative 4
-        ArrayList<TTTuple> exp3alt4crit1List = new ArrayList<>();
-        exp3alt4crit1List.add(new TTTuple("3", 5, 0, 3));
-        exp3alt4crit1List.add(new TTTuple("4", 5, 0, 4));
-
-        TT2HFLTS exp3alt4crit1 = new TT2HFLTS(exp3alt4crit1List);
-
-        ArrayList<TTTuple> exp3alt4crit2List = new ArrayList<>();
-        exp3alt4crit2List.add(new TTTuple("3", 5, 0, 3));
-
-        TT2HFLTS exp3alt4crit2 = new TT2HFLTS(exp3alt4crit2List);
-
-        ArrayList<TTTuple> exp3alt4crit3List = new ArrayList<>();
-        exp3alt4crit3List.add(new TTTuple("2", 5, 0, 2));
-
-        TT2HFLTS exp3alt4crit3 = new TT2HFLTS(exp3alt4crit3List);
-
-        // expert 3, alternative 5
-        ArrayList<TTTuple> exp3alt5crit1List = new ArrayList<>();
-        exp3alt5crit1List.add(new TTTuple("2", 5, 0, 2));
-
-        TT2HFLTS exp3alt5crit1 = new TT2HFLTS(exp3alt5crit1List);
-
-        ArrayList<TTTuple> exp3alt5crit2List = new ArrayList<>();
-        exp3alt5crit2List.add(new TTTuple("0", 5, 0, 0));
-        exp3alt5crit2List.add(new TTTuple("1", 5, 0, 1));
-
-        TT2HFLTS exp3alt5crit2 = new TT2HFLTS(exp3alt5crit2List);
-
-        ArrayList<TTTuple> exp3alt5crit3List = new ArrayList<>();
-        exp3alt5crit3List.add(new TTTuple("0", 5, 0, 0));
-
-        TT2HFLTS exp3alt5crit3 = new TT2HFLTS(exp3alt5crit3List);
-
-        ArrayList<TT2HFLTS> exp3alt1 = new ArrayList<>();
-        exp3alt1.add(exp3alt1crit1);
-        exp3alt1.add(exp3alt1crit2);
-        exp3alt1.add(exp3alt1crit3);
-
-        ArrayList<TT2HFLTS> exp3alt2 = new ArrayList<>();
-        exp3alt2.add(exp3alt2crit1);
-        exp3alt2.add(exp3alt2crit2);
-        exp3alt2.add(exp3alt2crit3);
-
-        ArrayList<TT2HFLTS> exp3alt3 = new ArrayList<>();
-        exp3alt3.add(exp3alt3crit1);
-        exp3alt3.add(exp3alt3crit2);
-        exp3alt3.add(exp3alt3crit3);
-
-        ArrayList<TT2HFLTS> exp3alt4 = new ArrayList<>();
-        exp3alt4.add(exp3alt4crit1);
-        exp3alt4.add(exp3alt4crit2);
-        exp3alt4.add(exp3alt4crit3);
-
-        ArrayList<TT2HFLTS> exp3alt5 = new ArrayList<>();
-        exp3alt5.add(exp3alt5crit1);
-        exp3alt5.add(exp3alt5crit2);
-        exp3alt5.add(exp3alt5crit3);
-
-        ArrayList<ArrayList<TT2HFLTS>> exp3 = new ArrayList<>();
-        exp3.add(exp3alt1);
-        exp3.add(exp3alt2);
-        exp3.add(exp3alt3);
-        exp3.add(exp3alt4);
-        exp3.add(exp3alt5);
-
-        ArrayList<ArrayList<ArrayList<TT2HFLTS>>> expEstAll = new ArrayList<>();
-        expEstAll.add(exp1);
-        expEstAll.add(exp2);
-        expEstAll.add(exp3);
+        // 2. read estimates from file
+        int NUMBER_OF_EXPERTS = ttjsonModel.getExperts().size();
+        int NUMBER_OF_ALTERNATIVES = ttjsonModel.getAlternatives().size();
+        int NUMBER_OF_CRITERIA = ttjsonModel.getCriteria().size();
+
+        Map<String, List<TTExpertEstimationsModel>> fileEstimations = ttjsonModel.getEstimations();
+
+        Map<String, List<TTExpertEstimationsModel>> treeMap = new TreeMap<String, List<TTExpertEstimationsModel>>(
+                (Comparator<String>) (o1, o2) -> {
+                    int num1 = Integer.parseInt(o1.replaceAll("[\\D]", ""));
+                    int num2 = Integer.parseInt(o2.replaceAll("[\\D]", ""));
+                    return num1 - num2;
+                }
+        );
+
+        treeMap.putAll(fileEstimations);
+
+        ArrayList<ArrayList<ArrayList<TT2HFLTS>>> expEstimationsAll = new ArrayList<>();
+
+        for(Map.Entry<String, List<TTExpertEstimationsModel>> el:fileEstimations.entrySet()){
+            ArrayList<ArrayList<TT2HFLTS>> expAll = new ArrayList<>();
+            el.getValue().sort((Comparator<TTExpertEstimationsModel>) (o1, o2) -> {
+                int num1 = Integer.parseInt(o1.getAlternativeID().replaceAll("[\\D]", ""));
+                int num2 = Integer.parseInt(o2.getAlternativeID().replaceAll("[\\D]", ""));
+                return num1 - num2;
+            });
+            // loop though sorted by alternative
+            for (TTExpertEstimationsModel model: el.getValue()){
+                model.getCriteria2Estimation().sort((Comparator<TTCriteriaEstimationsModel>) (o1, o2) -> {
+                    int num1 = Integer.parseInt(o1.getCriteriaID().replaceAll("[\\D]", ""));
+                    int num2 = Integer.parseInt(o2.getCriteriaID().replaceAll("[\\D]", ""));
+                    return num1 - num2;
+                });
+                ArrayList<TT2HFLTS> expToAltToCrit = new ArrayList<>();
+                // loop though sorted by criteria
+                for (TTCriteriaEstimationsModel criterion: model.getCriteria2Estimation()){
+                    List<String> val = criterion.getEstimation();
+                    ArrayList<TTTuple> expToAltToCritTTuple = new ArrayList<>();
+                    TTScaleModel scale= ttjsonModel.getScales()
+                            .stream()
+                            .filter(x -> x.getScaleID().equals(criterion.getScaleID()))
+                            .findFirst()
+                            .get();
+                    int scaleSize = scale.getLabels().size();
+                    for (String label: val){
+                        expToAltToCritTTuple.add(new TTTuple(label, scaleSize, 0, scale.getLabels().indexOf(label)));
+                    }
+                    expToAltToCrit.add(new TT2HFLTS(expToAltToCritTTuple));
+                }
+                expAll.add(expToAltToCrit);
+            }
+            expEstimationsAll.add(expAll);
+        }
 
         // 2. We need to aggregate value for each alternative for each expert (so collapse
         // all estimates by criteria)
@@ -494,7 +183,7 @@ public class TT2HFLTSCoordinator {
 
         ArrayList<ArrayList<TT2HFLTS>> aggEstAll = new ArrayList<>();
 
-        for (ArrayList<ArrayList<TT2HFLTS>> singleExpertMatrix: expEstAll){
+        for (ArrayList<ArrayList<TT2HFLTS>> singleExpertMatrix: expEstimationsAll){
             ArrayList<TT2HFLTS> aggregatedForSingle = new ArrayList<>();
             for (ArrayList<TT2HFLTS> alternativePerCriteriaList: singleExpertMatrix){
                 TT2HFLTS aggRes = tt2HFLTSMHTWAOperatorFinal.calculate(alternativePerCriteriaList, criteriaWeights, 7);
