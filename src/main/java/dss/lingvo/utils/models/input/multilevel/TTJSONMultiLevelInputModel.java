04a92a84020c411f0b35449b5d7c06cd27b52a4d
Alexander Demidovskij <alexander.demidovskij@intel.com>
2017-05-03 20:10:10 +0300

    removed duplication for JSON models

--- src/main/java/dss/lingvo/utils/models/input/multilevel/TTJSONMultiLevelInputModel.java
+++ src/main/java/dss/lingvo/utils/models/input/multilevel/TTJSONMultiLevelInputModel.java
@@ -3,80 +3,15 @@ package dss.lingvo.utils.models.input.multilevel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dss.lingvo.utils.models.input.*;
 import dss.lingvo.utils.models.input.TTCriteriaModel;
+import dss.lingvo.utils.models.input.common.TTCommonInputModel;
+import dss.lingvo.utils.models.input.singlelevel.TTJSONInputModel;
 
 import java.util.List;
 import java.util.Map;
 
-public class TTJSONMultiLevelInputModel {
+public class TTJSONMultiLevelInputModel extends TTCommonInputModel{
     @JsonProperty("criteria")
     private Map<String, List<TTCriteriaModel>> criteria;
-    private List<TTAlternativeModel> alternatives;
-    private List<TTScaleModel> scales;
-    private List<TTAbstractionLevelModel> abstractionLevels;
-    private List<TTExpertModel> experts;
-    @JsonProperty("estimations")
-    private Map<String, List<TTExpertEstimationsModel>> estimations;
-    @JsonProperty("abstractionLevelWeights")
-    private Map<String, Float> abstractionLevelWeights;
-    @JsonProperty("expertWeightsRule")
-    private Map<String, Float> expertWeightsRule;
-
-
-    public List<TTAlternativeModel> getAlternatives() {
-        return alternatives;
-    }
-
-    public void setAlternatives(List<TTAlternativeModel> alternatives) {
-        this.alternatives = alternatives;
-    }
-
-    public List<TTScaleModel> getScales() {
-        return scales;
-    }
-
-    public void setScales(List<TTScaleModel> scales) {
-        this.scales = scales;
-    }
-
-    public List<TTAbstractionLevelModel> getAbstractionLevels() {
-        return abstractionLevels;
-    }
-
-    public void setAbstractionLevels(List<TTAbstractionLevelModel> abstractionLevels) {
-        this.abstractionLevels = abstractionLevels;
-    }
-
-    public List<TTExpertModel> getExperts() {
-        return experts;
-    }
-
-    public void setExperts(List<TTExpertModel> experts) {
-        this.experts = experts;
-    }
-
-    public Map<String, List<TTExpertEstimationsModel>> getEstimations() {
-        return estimations;
-    }
-
-    public void setEstimations(Map<String,  List<TTExpertEstimationsModel>> estimations) {
-        this.estimations = estimations;
-    }
-
-    public Map<String, Float> getAbstractionLevelWeights() {
-        return abstractionLevelWeights;
-    }
-
-    public void setAbstractionLevelWeights(Map<String, Float> abstractionLevelWeights) {
-        this.abstractionLevelWeights = abstractionLevelWeights;
-    }
-
-    public Map<String, Float> getExpertWeightsRule() {
-        return expertWeightsRule;
-    }
-
-    public void setExpertWeightsRule(Map<String, Float> expertWeightsRule) {
-        this.expertWeightsRule = expertWeightsRule;
-    }
 
     public Map<String, List<TTCriteriaModel>> getCriteria() {
         return criteria;
--- src/main/java/dss/lingvo/utils/models/input/singlelevel/TTJSONInputModel.java
+++ src/main/java/dss/lingvo/utils/models/input/singlelevel/TTJSONInputModel.java
@@ -2,22 +2,13 @@ package dss.lingvo.utils.models.input.singlelevel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dss.lingvo.utils.models.input.*;
+import dss.lingvo.utils.models.input.common.TTCommonInputModel;
 
 import java.util.List;
 import java.util.Map;
 
-public class TTJSONInputModel {
+public class TTJSONInputModel extends TTCommonInputModel{
     private List<TTCriteriaModel> criteria;
-    private List<TTAlternativeModel> alternatives;
-    private List<TTScaleModel> scales;
-    private List<TTAbstractionLevelModel> abstractionLevels;
-    private List<TTExpertModel> experts;
-    @JsonProperty("estimations")
-    private Map<String, List<TTExpertEstimationsModel>> estimations;
-    @JsonProperty("abstractionLevelWeights")
-    private Map<String, Float> abstractionLevelWeights;
-    @JsonProperty("expertWeightsRule")
-    private Map<String, Float> expertWeightsRule;
 
     public List<TTCriteriaModel> getCriteria() {
         return criteria;
@@ -25,61 +16,5 @@ public class TTJSONInputModel {
 
     public void setCriteria(List<TTCriteriaModel> criteria) {
         this.criteria = criteria;
-    }
-
-    public List<TTAlternativeModel> getAlternatives() {
-        return alternatives;
-    }
-
-    public void setAlternatives(List<TTAlternativeModel> alternatives) {
-        this.alternatives = alternatives;
-    }
-
-    public List<TTScaleModel> getScales() {
-        return scales;
-    }
-
-    public void setScales(List<TTScaleModel> scales) {
-        this.scales = scales;
-    }
-
-    public List<TTAbstractionLevelModel> getAbstractionLevels() {
-        return abstractionLevels;
-    }
-
-    public void setAbstractionLevels(List<TTAbstractionLevelModel> abstractionLevels) {
-        this.abstractionLevels = abstractionLevels;
-    }
-
-    public List<TTExpertModel> getExperts() {
-        return experts;
-    }
-
-    public void setExperts(List<TTExpertModel> experts) {
-        this.experts = experts;
-    }
-
-    public Map<String, List<TTExpertEstimationsModel>> getEstimations() {
-        return estimations;
-    }
-
-    public void setEstimations(Map<String,  List<TTExpertEstimationsModel>> estimations) {
-        this.estimations = estimations;
-    }
-
-    public Map<String, Float> getAbstractionLevelWeights() {
-        return abstractionLevelWeights;
-    }
-
-    public void setAbstractionLevelWeights(Map<String, Float> abstractionLevelWeights) {
-        this.abstractionLevelWeights = abstractionLevelWeights;
-    }
-
-    public Map<String, Float> getExpertWeightsRule() {
-        return expertWeightsRule;
-    }
-
-    public void setExpertWeightsRule(Map<String, Float> expertWeightsRule) {
-        this.expertWeightsRule = expertWeightsRule;
     }
 }
